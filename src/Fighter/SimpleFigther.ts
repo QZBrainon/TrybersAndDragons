fead85eb7688f9c1868b4245c03fd53be0d5e4ee
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-10-22 17:28:05 -0300

    Req08 - OK

---
+++ src/Fighter/SimpleFigther.ts
@@ -0,0 +1,9 @@
+import Fighter from './Fighter';
+
+export default interface SimpleFighter extends Fighter {
+  lifePoints: number;
+  strength: number;
+
+  attack(enemy:SimpleFighter):void
+  receiveDamage(attackPoints:number):number
+}
--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,3 +1,5 @@
 import Fighter from './Fighter';
+import SimpleFighter from './SimpleFigther';
 
+export { SimpleFighter };
 export default Fighter;
