9b2816fd7f69442c1bb1542bdc65b77b65d59504
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-10-22 11:55:58 -0300

    Added level up

--- src/Character.ts
+++ src/Character.ts
@@ -75,9 +75,13 @@ export default class Character implements Fighter {
 
   levelUp(): void {
     this._maxLifePoints += getRandomInt(1, 10);
+    if (this._maxLifePoints > this._race.maxLifePoints) { 
+      this._maxLifePoints = this._race.maxLifePoints; 
+    }
     this._strength += getRandomInt(1, 10);
     this._dexterity += getRandomInt(1, 10);
     this._defense += getRandomInt(1, 10);
     this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
   }
 }
