ce417529b6da04230c683f2af1d9b93f358de574
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-10-23 14:01:46 -0300

    Req10 - OK

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,19 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    private fighter1: Fighter,
+    private fighter2:Fighter,
+  ) {
+    super(fighter1);
+  }
+
+  fight(): number {
+    while (this.fighter1.lifePoints > 0 && this.fighter2.lifePoints > 0) {
+      this.fighter1.attack(this.fighter2);
+      this.fighter2.attack(this.fighter1);
+    }
+    return super.fight();
+  }
+}
